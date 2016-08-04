d6f758f3b65ee9aee9214d418432046aad0b7f2c
Crash Springfield <ccrsh@riseup.net>
2016-08-03 22:17:50 -0400

    working button

---
+++ js/card-dealer.js
@@ -0,0 +1,19 @@
+var cardDealer = angular.module('card-dealer', []);
+
+cardDealer.controller('mainCtrl', function($scope) {
+
+  var deck = ['death', 'justice', 'strength', 'temperance', 'the_chariot',
+              'the_devil', 'the_emperor', 'the_empress', 'the_fool',
+              'the_hanged_man', 'the_hermit', 'the_hierophant','the_high_priestess',
+              'the_lovers', 'the_magician', 'the_moon', 'the_star', 'the_sun',
+              'the_tower', 'the_world', 'wheel_of_fortune'];
+
+
+  $scope.deal = deck[Math.floor(Math.random() * 21)];
+
+  $scope.dealAgain = function() {
+    $scope.deal = deck[Math.floor(Math.random() * 21)];
+    console.log('button clicked')
+  };
+
+});
